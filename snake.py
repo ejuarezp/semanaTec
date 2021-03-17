f3be811bd1c9532501bca15a7f20db70f9e3ac88
ejuarezp <edjuarezp@itesm.mx>
2021-03-17 12:02:28 -0600

    Código cumple con el estándar PEP8

--- snake.py
+++ snake.py
@@ -9,7 +9,8 @@ Exercises
 
 """
 
-from turtle import *
+from turtle import update, clear, ontimer, setup, hideturtle, \
+    tracer, listen, onkey, done
 from random import randrange
 from freegames import square, vector
 
@@ -17,14 +18,17 @@ food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+
 def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
 
+
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
+
 
 def move():
     "Move snake forward one segment."
@@ -53,6 +57,7 @@ def move():
     square(food.x, food.y, 9, 'green')
     update()
     ontimer(move, 100)
+
 
 setup(420, 420, 370, 0)
 hideturtle()
